f4fda3547246c25fd6b6ecc95058ba24b568cc74
shacharracheli <r0583237001@gmail.com>
2025-03-31 00:46:33 +0300

    footer2

--- client/ReactClient/src/components/footer.tsx
+++ client/ReactClient/src/components/footer.tsx
@@ -5,46 +5,39 @@ import TwitterIcon from '@mui/icons-material/Twitter';
 
 const Footer = () => {
     return (
-        <footer>
-            <Box 
-                sx={{
-                    backgroundColor: 'white', // שינוי צבע הרקע ללבן
-                    color: 'black', // שינוי צבע הטקסט לשחור כדי שיהיה קריא
-                    textAlign: 'center',
-                    padding: 2,
-                    position: 'absolute', // שינוי ל-fixed כדי להדביק אותו לתחתית
-                    bottom: 0,
-                    left: 0, // הוספת left כדי להדביק את הפוטר לשמאל
-                    right: 0, 
-                    width: '100%', 
-                }}
-            >
-                <Typography variant="body1">
-                    © {new Date().getFullYear()} Pick A Pic. All rights reserved.
-                </Typography>
-                <Box sx={{ marginTop: 1 }}>
-                    <IconButton 
-                        color="inherit" 
-                        href="https://www.youtube.com" 
-                        target="_blank"
-                    >
-                        <YouTubeIcon />
-                    </IconButton>
-                    <IconButton 
-                        color="inherit" 
-                        href="https://www.facebook.com" 
-                        target="_blank"
-                    >
-                        <FacebookIcon />
-                    </IconButton>
-                    <IconButton 
-                        color="inherit" 
-                        href="https://www.twitter.com" 
-                        target="_blank"
-                    >
-                        <TwitterIcon />
-                    </IconButton>
-                </Box>
+        <footer style={{
+            textAlign: 'center',
+            backgroundColor: '#0b0f17',
+            color: 'white',
+            padding: '40px 20px',
+            position: 'relative',
+            fontFamily: '"Plus Jakarta Sans", Arial, Helvetica, sans-serif',
+        }}>
+            <Typography variant="body1">
+                © {new Date().getFullYear()} Pick A Pic. All rights reserved.
+            </Typography>
+            <Box sx={{ marginTop: 1 }}>
+                <IconButton 
+                    color="inherit" 
+                    href="https://www.youtube.com" 
+                    target="_blank"
+                >
+                    <YouTubeIcon />
+                </IconButton>
+                <IconButton 
+                    color="inherit" 
+                    href="https://www.facebook.com" 
+                    target="_blank"
+                >
+                    <FacebookIcon />
+                </IconButton>
+                <IconButton 
+                    color="inherit" 
+                    href="https://www.twitter.com" 
+                    target="_blank"
+                >
+                    <TwitterIcon />
+                </IconButton>
             </Box>
         </footer>
     );
