ba839581e0fba9b24651de8fa5307ae3b4eb6ca8
shacharracheli <r0583237001@gmail.com>
2025-05-01 00:17:50 +0300

    vvvv

--- client/ReactClient/src/components/Pictures/vote.tsx
+++ client/ReactClient/src/components/Pictures/vote.tsx
@@ -1,75 +1,76 @@
-import {  IconButton } from "@mui/material"
+import { IconButton } from "@mui/material"
 import { getUserIdByToken } from "../store/getFromToken"
 import { useDispatch, useSelector } from "react-redux";
 import { AppDispatch, RootState } from "../redux/store";
 import { addVote, deleteVote } from "../redux/imageSlice";
-import {  useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 
 import AddIcon from '@mui/icons-material/Add';
 import HorizontalRuleIcon from '@mui/icons-material/HorizontalRule';
 import axios from "axios";
 const apiUrl = import.meta.env.VITE_API_URL;
 
-const Vote=({imageId,challengeId}:{imageId:number,challengeId:number})=>{
-const userId=getUserIdByToken();
-const dispatch=useDispatch<AppDispatch>();
-// const vote=
-useSelector((state:RootState)=>state.images.imagesByChallenge);
-  const challenge = useSelector((state: RootState) => state.challenges.selectedChallenge);
-  const [isVote,setIsVote]=useState<boolean>();
-  const [token,_]=useState<string|null>(sessionStorage.getItem('token'))
-//   const isTokenPresent = !!token;
-  useEffect(()=>{
-    const checkUserVote=async ()=>{
-try{
-    const response=await axios.get(`${apiUrl}/api/Vote/HasVoted`,{
-        params:{ImageId:imageId,UserId:userId},
-        headers: {
-            Authorization: `Bearer ${token}`,
-            "Content-Type": "application/json",
+const Vote = ({ imageId, challengeId }: { imageId: number, challengeId: number }) => {
+    const userId = getUserIdByToken();
+    const dispatch = useDispatch<AppDispatch>();
+    // const vote=
+    useSelector((state: RootState) => state.images.imagesByChallenge);
+    const challenge = useSelector((state: RootState) => state.challenges.selectedChallenge);
+    const [isVote, setIsVote] = useState<boolean>();
+    const [token, _] = useState<string | null>(sessionStorage.getItem('token'))
+    //   const isTokenPresent = !!token;
+    useEffect(() => {
+        const checkUserVote = async () => {
+            try {
+                const response = await axios.get(`${apiUrl}/api/Vote/HasVoted`, {
+                    params: { ImageId: imageId, UserId: userId },
+                    headers: {
+                        Authorization: `Bearer ${token}`,
+                        "Content-Type": "application/json",
+                    }
+                })
+                setIsVote(response.data);
+            } catch (e) {
+                console.error("there was an error");
+            }
         }
-    })
-    setIsVote(response.data);
-
-}catch(e){
-    console.error("there was an error");
-    
-}
+        checkUserVote();
+    }, [token])
+    const clickAddVote = () => {
+        dispatch(addVote({ userId, imageId, challengeId }))
+        setIsVote(false)
+        console.log(isVote+"kkkkkkkkkkkk");
+        
     }
-    checkUserVote();
-},[token])
-const clickAddVote=()=>{
-    dispatch(addVote({userId,imageId,challengeId}))
-    setIsVote(true)
-}
-const clickDeleteVote=()=>{
-    dispatch(deleteVote({userId,imageId,challengeId}))
-    setIsVote(false)
-}
-const isChallengeActive = challenge?.status ? true : false;
-console.log(isChallengeActive);
-console.log(challenge);
+    const clickDeleteVote = () => {
+        dispatch(deleteVote({ userId, imageId, challengeId }))
+        setIsVote(true)
+        console.log(isVote+"kkkkkkkkkkkk");
+    }
+    const isChallengeActive = challenge?.status ? true : false;
+    console.log(isChallengeActive);
+    console.log(challenge);
 
 
-return(<>
- <IconButton 
-                onClick={clickAddVote} 
-                sx={{ color: "purple", opacity: isVote ? 1 : 0.5 }} 
-                // disabled={!isVote}
-                disabled={!isVote || !isChallengeActive||!token} 
-            >
-                <AddIcon />
-            </IconButton>
-            <IconButton 
-                onClick={clickDeleteVote} 
-                sx={{ color: "purple", opacity: isVote ? 0.5 : 1 }} 
-                // disabled={isVote}
-                disabled={isVote || !isChallengeActive||!token} 
-            >
-                <HorizontalRuleIcon />
-            </IconButton>
-            {/* <Box></Box> */}
-  {/* {isVote ? (
+    return (<>
+        <IconButton
+            onClick={clickAddVote}
+            sx={{ color: "purple", opacity: isVote ? 1 : 0.5 }}
+            // disabled={!isVote}
+            disabled={!isVote || !isChallengeActive || !token}
+        >
+            <AddIcon />
+        </IconButton>
+        <IconButton
+            onClick={clickDeleteVote}
+            sx={{ color: "purple", opacity: isVote ? 0.5 : 1 }}
+            // disabled={isVote}
+            disabled={isVote || !isChallengeActive || !token}
+        >
+            <HorizontalRuleIcon />
+        </IconButton>
+        {/* <Box></Box> */}
+        {/* {isVote ? (
                 <IconButton onClick={clickAddVote} sx={{color:"purple"}}>
                     <AddIcon />
                 </IconButton>
@@ -79,7 +80,7 @@ return(<>
                 </IconButton>
             )}
             <Box></Box> */}
-</>)
+    </>)
 }
 export default Vote
 
